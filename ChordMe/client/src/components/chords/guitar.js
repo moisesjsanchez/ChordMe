a820d237ca01f519c3dc0c04bc14461d636ec951
moisesjsanchez <moijsanchez@gmail.com>
2019-12-24 01:38:57 -0800

    created rest api for chord creation

---
+++ ChordMe/client/src/components/chords/guitar.js
@@ -0,0 +1,13 @@
+import React, { Components } from 'react'
+
+class Guitar extends React.Component{
+    render(){
+        return(
+            <div>
+                This is the Guitar
+            </div>
+        )
+    }
+}
+
+export default Guitar
--- ChordMe/user/routes.py
+++ ChordMe/user/routes.py
@@ -5,13 +5,36 @@ from ChordMe.models import Chord
 user = Blueprint('user', __name__)
 
 #api for registered users chord manipulation 
-@user.route('/my-chords')
-def test():
-    return ""
-
 @user.route('/user', methods=['GET'])
 def select_chord():
-    return ""
+    chord = Chord.query.filter_by(id=id).first()
+
+    if not chord:
+        return jsonify('message':'chord not found')
+        
+    chord_data['id'] = chord.id
+    chord_data['notes'] = chord.notes
+    chord_data['string_names'] = chord.string_names
+    chord_data['chord_name'] = chord.chord_name
+    
+    output.append(chord_data)
+
+    return jsonify({'chords': output})
+
+@user.route('/user', methods=['GET'])
+def select_chords():
+    chords = Chord.query.all()
+
+    output = []
+    for chord in chords:
+        chord_data = {}
+        chord_data['id'] = chord.id
+        chord_data['notes'] = chord.notes
+        chord_data['string_names'] = chord.string_names
+        chord_data['chord_name'] = chord.chord_name
+        output.append(chord_data)
+
+    return jsonify({'chords': output})
 
 @user.route('/user', methods=['POST'])
 def create_chord():
@@ -27,8 +50,26 @@ def create_chord():
 
 @user.route('/user', methods=['PUT'])
 def edit_chord():
-    return ""
+
+    chord = Chord.query.filter_by(id=id).first()
+
+    if not chord:
+        return jsonify('message':'chord not found')
+    
+    chord = Chord(chord_name=chord_data['chord_name'], string_names=chord_data['string_names'], 
+    notes=chord_data['notes'])
+    db.session.commit()
+
+    return jsonify('message':'Chord has been edited')
 
 @user.route('/user', methods=['DELETE'])
 def delete_chord():
-    return ""
+
+    chord = Chord.query.filter_by(id=id).first()
+        if not chord:
+        return jsonify('message':'chord not found')
+    
+    db.session.delete(chord)
+    db.session.commit()
+
+    return jsonify('message':'Chord has been deleted')
