36fbe0b3a66fbb07e5691c8ba09de0d8e42fc9cb
moisesjsanchez <moijsanchez@gmail.com>
2020-01-06 02:26:07 -0800

    Added more to User schema

--- ChordMe/models.py
+++ ChordMe/models.py
@@ -1,12 +1,17 @@
 from ChordMe import db
-from flask import current_app
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20),unique=True, nullable=False)
+    username = db.Column(db.String(20), unique=True, nullable=True)
+    email = db.Column(db.String, nullable=False)
+    password = db.Column(db.String(80), nullable=True)
+    admin = db.Column(db.Boolean)
+    public_id = db.Column(db.String(50), unique=True)
+
 
 class Chord(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     notes = db.Column(db.String, nullable=False)
     string_names = db.Column(db.String, nullable=False)
-    chord_name =  db.Column(db.String, nullable=False)
+    chord_name = db.Column(db.String, nullable=False)
